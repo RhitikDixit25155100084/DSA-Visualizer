9b95b6d83eeab91ad171aa7d53fda8422f970ff6
RhitikDixit2515510084 <rhitikdixit668@gmail.com>
2026-04-24 23:49:23 +0530

    Delete js directory

--- js/array.js
+++
@@ -1,4 +0,0 @@
-let arr=[];
-function insert(){arr.push(document.getElementById("val").value);render();}
-function render(){let c=document.getElementById("container");c.innerHTML="";
-arr.forEach(v=>{let d=document.createElement("div");d.className="box";d.innerText=v;c.appendChild(d);});}
--- js/queue.js
+++
@@ -1,4 +0,0 @@
-let q=[];
-function enqueue(){q.push(document.getElementById("val").value);render();}
-function render(){let c=document.getElementById("queue");c.innerHTML="";
-q.forEach(v=>{let d=document.createElement("div");d.className="box";d.innerText=v;c.appendChild(d);});}
